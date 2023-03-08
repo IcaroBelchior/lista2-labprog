e10545af2f8074147263f84a50e1ce90780dc89e
IcaroBelchior <124515130+IcaroBelchior@users.noreply.github.com>
2023-03-08 09:22:58 -0300

    Create questão14.c

---
+++ questão14.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int somatorio(int inicio, int fim){
+    int cont = 0, i;
+  
+    for(i = inicio; i <= fim; i++){
+        if(i % 3 == 0){
+            cont++;
+        }
+    }
+  
+    return cont;
+}
+
+int main(){
+    int a, b, i, cont = 0;
+  
+    printf("Digite o valor de A: ");scanf("%d", &a);  
+    printf("Digite o valor de B: ");scanf("%d", &b);
+  
+    if(a > b){
+        cont = somatorio(b, a);
+    }
+    else{
+        cont = somatorio(a, b);
+    }
+  
+    printf("Quantidade de números divisíveis por 3 entre %d e %d: %d\n", a, b, cont);
+  
+    return 0;
+}
+
