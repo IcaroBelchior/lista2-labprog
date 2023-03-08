73651896a72a5dadc5e236327f554f2075625bb6
IcaroBelchior <124515130+IcaroBelchior@users.noreply.github.com>
2023-03-08 09:21:11 -0300

    Create questão13.c

---
+++ questão13.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int somatorio(int inicio, int fim){
+    int soma = 0, i;
+  
+    for(i = inicio; i <= fim; i++){
+        if(i % 2 == 0){
+            soma += i;
+        }
+    }
+  
+    return soma;
+}
+
+int main(){
+    int a, b, i, soma = 0;
+    printf("Digite o valor de A: ");scanf("%d", &a);
+  
+    printf("Digite o valor de B: ");scanf("%d", &b);
+  
+    if(a > b){
+        soma = somatorio(b, a);
+    }
+    else{
+        soma = somatorio(a, b);
+    }
+  
+    printf("Soma dos numeros pares entre %d e %d: %d\n", a, b, soma);
+  
+    return 0;
+  
+}
